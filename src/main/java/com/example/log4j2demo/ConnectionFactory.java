41ec613fb10554ea2a7b9828b1f4d70bb9ac32b9
srinivashyd77 <srinivashyd77@gmail.com>
2019-01-27 16:59:38 +0530

    spring boot log4j2 JDBC

---
+++ src/main/java/com/example/log4j2demo/ConnectionFactory.java
@@ -0,0 +1,25 @@
+package com.example.log4j2demo;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class ConnectionFactory {
+
+   private static BasicDataSource dataSource;
+
+   private ConnectionFactory() {
+   }
+
+   public static Connection getConnection() throws SQLException {
+      if (dataSource == null) {
+         dataSource = new BasicDataSource();
+         dataSource.setUrl("jdbc:mysql://localhost:3306/vasu");
+         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+         dataSource.setUsername("root");
+         dataSource.setPassword("mysql");
+      }
+      return dataSource.getConnection();
+   }
+}
